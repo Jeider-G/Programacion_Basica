bae058ab83ad625bf2a31d904308474e5dea0c4b
Jeider-G <ljeider618@gmail.com>
2025-12-02 05:53:35 -0500

    Update Actividad_VectorA(con apuntadores).cpp

--- Actividad_VectorA(con apuntadores).cpp
+++ Actividad_VectorA(con apuntadores).cpp
@@ -3,7 +3,16 @@ using namespace std;
 
 int main() {
     const int N = 10;
-    int A[N] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int A[N];
+
+    
+    cout << "Ingrese los " << N << " valores del vector:\n";
+
+    for (int i = 0; i < N; i++) {
+        cout << "A[" << i << "] = ";
+        cin >> A[i];     
+    }
+
 
     cout << "1) MOSTRAR VECTOR USANDO INDICES (i)\n";
     for (int i = 0; i < N; i++) {
